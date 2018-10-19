dc2a4ebdb4e15f55a00f6f537c7c43a42cac7ed0
akhil thankachen <akhilthankachen1234@gmail.com>
2018-10-19 11:23:59 +0530

    pages as jpeg added

--- static/js/content.js
+++ static/js/content.js
@@ -1,4 +1,32 @@
+//init all pages
+function init(){
+    $("#two").css({"display":"none"});
+    $("#three").css({"display":"none"});
+    $("#four").css({"display":"none"});
+    $("#five").css({"display":"none"});
+}
+
+//function init css of one
+function initOne(){
+    $("#one").css({
+        "background-color":"red",
+        "height":"100%",
+        "width":"50%",
+        "display":"block",
+        "position":"absolute",
+        "margin":"auto",
+        "left":"0",
+        "right":"0",
+        "top":"0",
+        "bottom":"0",
+        "z-index":"99"
+    });
+}
+
+
 //function executes when dom is ready
 $( document ).ready(function() {
-    alert( "ready!" );
+    //init
+    init();
+    initOne();
 });
